a8c42b3e616a8a9302e8c84bdd14a6e71359bf72
kidshenlong <michaelpm91@googlemail.com>
2015-11-23 22:06:01 +0000

    Andreism routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,12 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$app->get('/', 'AndreismController@index');
 
-$app->get('/', function () use ($app) {
-    return $app->welcome();
-});
+$app->get('/andreism', function () use ($app) { return redirect('/'); });
+
+$app->get('/andreism.json', 'AndreismController@indexJson');
+
+$app->get('/andreism/{id}', 'AndreismController@show');
+
+$app->get('/andreism/{id}.json', 'AndreismController@showJson');
